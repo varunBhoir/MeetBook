9560931abc87bb6bb4bc04b366ac7e8a45cc8999
bhoirvarun6@gmail.com <bhoirvarun6@gmail.com>
2021-02-16 17:00:38 +0530

    Initial commit

---
+++ MeetBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeetBook
+//
+//  Created by varun bhoir on 16/02/21.
+//  Copyright © 2021 varun bhoir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
