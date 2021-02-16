6dd072b08ecb649a62457f6f87f3afcbd7bcbbe5
varunBhoir <72748623+varunBhoir@users.noreply.github.com>
2021-02-16 17:11:40 +0530

    Update ContentView.swift
    Just change the text string to shut up

--- MeetBook/ContentView.swift
+++ MeetBook/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I Love Riya")
+        VStack {
+            Text("I Love Riya")
+            Text("Just shut up")
+        }
     }
 }
 
